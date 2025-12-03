4ba724c0a110ddf3bf69d73fe366da29dabdfdb0
Obianuju-Sunday <obianujusunday43@gmail.com>
2025-12-03 16:40:18 +0100

    exercise 04 done

---
+++ 04-mad-lib/solution.js
@@ -0,0 +1,6 @@
+// Challenge 4 - Mad Lib
+var noun = prompt('Enter a noun:');
+var verb = prompt('Enter a verb:');
+var adjective = prompt('Enter an adjective:');
+var adverb = prompt('Enter an adverb:');
+console.log(`Have you seen the ${adjective} ${noun} ${verb} ${adverb}`);
