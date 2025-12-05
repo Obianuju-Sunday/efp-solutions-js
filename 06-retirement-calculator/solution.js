67b4eca3e7c37978e49f7b7eab3629580dd637db
Obianuju-Sunday <obianujusunday43@gmail.com>
2025-12-05 08:57:16 +0100

    exrecise 06 done

--- 06-retirement-calculator/solution.js
+++ 06-retirement-calculator/solution.js
@@ -1,2 +1,18 @@
 const currentYear = new Date().getFullYear();
 console.log(currentYear);
+
+var currentAge = Number(prompt('What is your current age?'));
+var retirementAge = Number(prompt('At what age would you like to retire?'));
+console.log(currentAge, retirementAge)
+
+var yearsUntilRetirement = retirementAge - currentAge;
+var retirementYear = currentYear + yearsUntilRetirement;
+
+if (yearsUntilRetirement > 0) {
+    console.log(`You have ${yearsUntilRetirement} years left until you can retire.`);
+    console.log(`It's ${currentYear}, so you can retire in ${retirementYear}.`);
+} else if (yearsUntilRetirement === 0) {
+    console.log(`You can retire this year, ${currentYear}.`);
+} else {
+    console.log(`The current year is ${currentYear} and so you should have retired in ${retirementYear}.`);
+}
