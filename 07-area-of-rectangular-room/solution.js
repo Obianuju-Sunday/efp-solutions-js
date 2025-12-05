b1016d24f194951dc449d1a8b2ef0074de1e74d1
Obianuju-Sunday <obianujusunday43@gmail.com>
2025-12-05 14:23:09 +0100

    restructuring

--- 07-area-of-rectangular-room/solution.js
+++ 07-area-of-rectangular-room/solution.js
@@ -1,9 +1,33 @@
 
 var userChoice = prompt("Do you want to enter dimensions in feet or meters? (Type 'feet' or 'meters')").toLowerCase();
 
-var length = Number(prompt(`What is the length of the room in ${userChoice}?`));
+if (userChoice !== 'feet' && userChoice !== 'meters') {
+    console.log("Invalid choice. Please type 'feet' or 'meters'.");
+    return;
+};
 
+var length = Number(prompt(`What is the length of the room in ${userChoice}?`));
 var width = Number(prompt(`What is the width of the room in ${userChoice}?`));
+
+
+if (isNaN(length) || isNaN(width)) {
+    console.log("Please enter valid numbers for length and width.");
+    return;
+};
+
+if (length <= 0 || width <= 0) {
+    console.log("Length and width must be positive numbers and greater than zero.");
+    return;
+};
+
+if (userChoice === 'feet') {
+
+    calculateAreaInFeet();
+} else {
+
+    calculateAreaInMeters();
+};
+
 
 function calculateAreaInMeters() {
     console.log(`You entered dimensions of ${length} by ${width} ${userChoice}.`)
@@ -20,24 +44,3 @@ function calculateAreaInFeet() {
     console.log(`Area in meters = ${squareFeet} * 0.09290304 = ${squareMeter} m²`);
     console.log('The formula for this conversion is m² = ft² × 0.09290304');
 }
-
-
-if (isNaN(length) || isNaN(width)) {
-    console.log("Please enter valid numbers for length and width.");
-}
-else if (length <= 0 || width <= 0) {
-    console.log("Length and width must be positive numbers and greater than zero.");
-}
-else if (userChoice === 'feet') {
-
-    calculateAreaInFeet();
-}
-else if (userChoice === 'meters') {
-    // Convert meters to feet
-    calculateAreaInMeters();
-}
-else {
-    console.log("Invalid choice. Please type 'feet' or 'meters'.");
-}
-
-
